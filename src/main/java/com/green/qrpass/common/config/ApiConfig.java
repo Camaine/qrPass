8bdc7f8709b2832b615934cce3ba0b0643f64c54
juwon kim <kang9290@gmail.com>
2022-01-11 18:22:05 +0900

    FEAT:AES 암호화 테스트 환경 구성

---
+++ src/main/java/com/green/qrpass/common/config/ApiConfig.java
@@ -0,0 +1,13 @@
+package com.green.qrpass.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class ApiConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+    }
+}
--- src/test/java/com/green/qrpass/QrPassApplicationTests.java
+++ src/test/java/com/green/qrpass/QrPassApplicationTests.java
@@ -1,13 +1,27 @@
 package com.green.qrpass;
 
+import com.green.qrpass.utils.AES256;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+@WebAppConfiguration
 @SpringBootTest
 class QrPassApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void dataEncryptTest(){
+        AES256 aes256 = new AES256();
+        try {
+            System.out.println("ENCRYPT : " + aes256.encrypt("test"));
+        }catch (Exception e){
+            System.out.println("ENCRYPT : FAIL" );
+        }
+
     }
 
 }
