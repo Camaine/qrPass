f1cc7a727d3ac4e988486f30ba85ff0d461b0ccb
juwon kim <kang9290@gmail.com>
2022-01-11 11:03:56 +0900

    Initial commit

---
+++ src/main/java/com/green/qrpass/QrPassApplication.java
@@ -0,0 +1,13 @@
+package com.green.qrpass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrPassApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QrPassApplication.class, args);
+    }
+
+}
